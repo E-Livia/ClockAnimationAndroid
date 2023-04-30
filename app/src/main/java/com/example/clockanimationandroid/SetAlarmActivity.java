525fb98e14a5d28ee174dbebb3bf8a9ee3c40c33
E-Livia <livia.popa.3@gmail.com>
2023-05-01 02:54:43 +0300

    created layout for set alarm

--- app/src/main/java/com/example/clockanimationandroid/SetAlarmActivity.java
+++ app/src/main/java/com/example/clockanimationandroid/SetAlarmActivity.java
@@ -1,6 +1,9 @@
 package com.example.clockanimationandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +11,13 @@ public class SetAlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
+
+        Button cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
